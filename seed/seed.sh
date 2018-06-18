6a2c62b1fb91d737161b3032d1d450fdacfdfde8
Ido Samuelson <ido@Idos-MacBook-Pro.local>
2018-06-18 15:50:37 -0600

    no need for nodeosd -p on seed

--- seed/seed.sh
+++ seed/seed.sh
@@ -4,13 +4,13 @@ if [ ! -d "/opt/eosio/bin/data-dir/state" ]; then
 	/opt/eosio/bin/genesis.sh
 else
 	echo "**** Running seed ****"
-	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e -p eosio
+	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e
 fi
 
 if [ $? -eq 2 ]
 then
    echo "**** Running seed + replay-blockchain ****"
-	 /opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e -p eosio --replay-blockchain --hard-replay-blockchain
+	 /opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e ---replay-blockchain --hard-replay-blockchain
 fi
 
 if [ $? -eq 2 ]
