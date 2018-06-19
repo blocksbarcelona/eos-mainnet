eb372f61761479399797f73e6ddb016380f7b892
Ido Samuelson <ido.samuelson@gmail.com>
2018-06-19 12:44:21 -0600

    improve seed.sh and config.ini

--- seed/seed.sh
+++ seed/seed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d "/opt/eosio/bin/data-dir/state" ]
+if [ ! -d "/opt/eosio/bin/data-dir/blocks" ]
 then
 	/opt/eosio/bin/genesis.sh
 else
@@ -10,13 +10,35 @@ fi
 
 if [ $? -eq 2 ]
 then
-	echo "**** Running seed + replay-blockchain ****"
-	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e --replay-blockchain --hard-replay-blockchain
+	if [ -d "/opt/eosio/bin/data-dir/blocks/reversible" ]
+	then
+		echo "*** remove reversible folder"
+		rm -rf /opt/eosio/bin/data-dir/blocks/reversible
+	fi
+
+	if [ -d "/opt/eosio/bin/data-dir/state" ]
+	then
+		echo "*** remove state folder"
+		rm -rf /opt/eosio/bin/data-dir/blocks/state
+	fi
+	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e
 fi
 
 if [ $? -eq 2 ]
 then
-	echo "**** Hard seed blockchain recovery ****"
+	echo "**** Running seed + replay-blockchain"
+	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e --replay-blockchain
+fi
+
+if [ $? -eq 2 ]
+then
+	echo "**** Running seed + hard-replay-blockchain ****"
+	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e --hard-replay-blockchain
+fi
+
+if [ $? -eq 2 ]
+then
+	echo "**** remove blocks and state folders ****"
 	rm -rf /opt/eosio/bin/data-dir/blocks/
 	rm -rf /opt/eosio/bin/data-dir/state/
 
