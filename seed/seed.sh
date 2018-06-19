14c30e19b2b1af475650b1448a6783fae97a84cf
Ido Samuelson <ido.samuelson@gmail.com>
2018-06-18 21:47:09 -0600

    fixed seed.sh

--- seed/seed.sh
+++ seed/seed.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-if [ ! -d "/opt/eosio/bin/data-dir/state" ]; then
+if [ ! -d "/opt/eosio/bin/data-dir/state" ]
+then
 	/opt/eosio/bin/genesis.sh
 else
 	echo "**** Running seed ****"
 	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e
 fi
 
-if [ $? -eq 2 ] then
-   echo "**** Running seed + replay-blockchain ****"
-	 /opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e ---replay-blockchain --hard-replay-blockchain
+if [ $? -eq 2 ]
+then
+	echo "**** Running seed + replay-blockchain ****"
+	/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e --replay-blockchain --hard-replay-blockchain
 fi
 
-if [ $? -eq 2 ] then
-   echo "**** Hard seed blockchain recovery ****"
-	 rm -rf /opt/eosio/bin/data-dir/blocks/
-	 rm -rf /opt/eosio/bin/data-dir/state/
+if [ $? -eq 2 ]
+then
+	echo "**** Hard seed blockchain recovery ****"
+	rm -rf /opt/eosio/bin/data-dir/blocks/
+	rm -rf /opt/eosio/bin/data-dir/state/
 
-	 /opt/eosio/bin/seed.sh
+	/opt/eosio/bin/seed.sh
 fi
